e72c654e584f6c4ef14febb26c1692a7355054a8
FragoIt <90487476+FragoIt@users.noreply.github.com>
2025-07-29 08:27:25 -0500

    Add .next/static/chunks/src_app_page_tsx_8d6d5d58._.js 1753795644584

---
+++ .next/static/chunks/src_app_page_tsx_8d6d5d58._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d8cdec79._.js",
+  "static/chunks/node_modules_e776313f._.js"
+],
+    source: "dynamic"
+});
